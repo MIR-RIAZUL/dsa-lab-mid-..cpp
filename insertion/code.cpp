6931cb5f18c416d7d31497c2563e333a5c2b66b1
MIR-RIAZUL <mirriazul859@gmail.com>
2025-09-14 19:34:19 +0600

    first commit

---
+++ insertion/code.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for(int i=1; i<n; i++)
+    {
+        int current = arr[i];
+        int prev = i-1;
+        while(prev>=0 && arr[prev]>current)
+        {
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = current;
+    }
+}
+
+int main()
+{
+    int n, m;
+   
+
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+     int array[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>array[i];
+    }
+    insertionSort(array, n);
+    cout<<"Sorted array: "; 
+    for(int i=0; i<n; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    return 0;
+}
---
+++ marge sort/1st.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+
+using namespace std;
+
+int main()
+{
+    cout<<"allah is grerat"<<endl;
+
+    return 0;
+}
